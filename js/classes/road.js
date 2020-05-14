9ae1a78c3b2ca981873e05d6b07f97b057f9b121
Michael Michaud <mmichaud3@yahoo.com>
2020-05-13 18:05:05 -0700

    initial commit

---
+++ js/classes/road.js
@@ -0,0 +1,45 @@
+class Road extends Phaser.GameObjects.Container {
+  constructor(config) {
+    super(config.scene);
+    this.scene = config.scene;
+    this.back = this.scene.add.image(0, 0, 'road');
+    this.add(this.back);
+    this.scene.add.existing(this);
+
+    //this.back.displayWidth = game.config.width * 0.5;
+    //this.back.scaleY = this.back.scaleX;
+    Align.scaleToGame(this.back, 0.5);
+
+    this.setSize(this.back.displayWidth, game.config.height);
+
+    this.lineGroup = this.scene.add.group();
+
+    this.count = 0;
+  }
+
+  makeLines() {
+    this.vSpace = this.displayHeight / 10;
+    for (var i = 0; i < 20; i++) {
+      var line = this.scene.add.image(this.x, this.vSpace * i, 'line');
+      line.oy = line.y;
+      this.lineGroup.add(line);
+    }
+  }
+
+  moveLines() {
+    this.lineGroup.children.iterate(
+      function (child) {
+        child.y += this.vSpace / 20;
+      }.bind(this)
+    );
+    this.count++;
+    if (this.count == 20) {
+      this.count = 0;
+      this.lineGroup.children.iterate(
+        function (child) {
+          child.y = child.oy;
+        }.bind(this)
+      );
+    }
+  }
+}
---
+++ js/sceneMain.js
@@ -0,0 +1,24 @@
+class SceneMain extends Phaser.Scene {
+  constructor() {
+    super('SceneMain');
+  }
+  preload() {
+    // load images and sounds
+    this.load.image('road', 'images/road.jpg');
+    this.load.spritesheet('cars', 'images/cars.png', {
+      frameWidth: 60,
+      frameHeight: 126,
+    });
+    this.load.image('line', 'images/line.png');
+  }
+  create() {
+    // define our objects
+    this.road = new Road({ scene: this });
+    this.road.x = game.config.width / 2;
+    this.road.makeLines();
+  }
+  update() {
+    //constant running loop
+    this.road.moveLines();
+  }
+}
